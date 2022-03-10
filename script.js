fab0feb1df72bae8fbd8555cd88c7a2499723818
Wellington Oliveira <jwell.oliveira@gmail.com>
2022-03-10 01:00:51 -0300

    atualização da imagnes pasta midia, atualização html e css

---
+++ script.js
@@ -0,0 +1,11 @@
+const btnmudar = document.querySelector("#btn"); // Captura o botão no DOM pelo id
+
+
+btnmudar.addEventListener("click", function () { // Adiciona um evento de click no botão capturado e assim que for clicado, executa a função.
+    
+    document.getElementById("img").src="./midia/img/homer.png";
+   
+     
+});
+  
+      
