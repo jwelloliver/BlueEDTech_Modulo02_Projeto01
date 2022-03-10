2ce1e91bfa9d010c0e3f80207a93f967f5ab8364
Wellington Oliveira <50851708+jwelloliver@users.noreply.github.com>
2022-03-10 11:00:52 -0300

    atualização do script, retirada de um erro na exibição da imagem

--- script.js
+++ script.js
@@ -5,27 +5,27 @@ const imagem = document.querySelector('#img')
 btnmudar.addEventListener('click', () =>{
    
     if(btnmudar.value == 'primeiro'){
-        imagem.src = './midia/img/familiasimpsons.png' 
-        nome.innerText = 'Familia' 
-        btnmudar.value = 'segundo'
+        imagem.src = './midia/img/homer.png';
+        nome.innerText = 'Homer';
+        btnmudar.value = 'segundo';
 
     } else if(btnmudar.value == 'segundo') { 
-        imagem.src = './midia/img/Marge.png'
-        nome.innerText = 'Marge' 
-        btnmudar.value = 'terceiro' 
+        imagem.src = './midia/img/Marge.png';
+        nome.innerText = 'Marge';
+        btnmudar.value = 'terceiro';
 
     } else if(btnmudar.value == 'terceiro') { 
-        imagem.src = './midia/img/lisa.png'
-        nome.innerText = 'lisa'
-        btnmudar.value = 'quarto'
+        imagem.src = './midia/img/lisa.png';
+        nome.innerText = 'lisa';
+        btnmudar.value = 'quarto';
     } else if(btnmudar.value == 'quarto') { 
-        imagem.src = './midia/img/barte.png'
-        nome.innerText = 'Barte'
-        btnmudar.value = 'quinto'
+        imagem.src = './midia/img/barte.png';
+        nome.innerText = 'Barte';
+        btnmudar.value = 'quinto';
     } else { 
-        imagem.src = './midia/img/homer.png'
-        nome.innerText = 'Homer'
-        btnmudar.value = 'primeiro'
+        imagem.src = './midia/img/familiasimpsons.png';
+        nome.innerText = 'Familia';
+        btnmudar.value = 'primeiro';
     }
 })
   
